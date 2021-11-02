d975526ddcd5dcb62d590a7ec47ddf4837f787a3
Kelvin Gitahi <kelvin.gitahi@student.moringaschool.com>
2021-11-02 11:01:02 +0300

    generating related components (#1)

--- front_end/mboga/src/app/app.module.ts
+++ front_end/mboga/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavComponent,
+    FooterComponent,
+    ProductsComponent,
+    LoginComponent,
+    SignupComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
