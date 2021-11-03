982062f02a675c6516ce22de787fbbe040268b83
tailorv <maxwellmuthomijr@gmail.com>
2021-11-03 15:19:55 +0300

    create signup page and login page

--- front_end/mboga/src/app/components/signup/signup.component.ts
+++ front_end/mboga/src/app/components/signup/signup.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
+  
   ngOnInit(): void {
   }
 
